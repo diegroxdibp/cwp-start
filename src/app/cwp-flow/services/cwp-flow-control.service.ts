07dce311bc8fc31ba4ac622f32e3653a4b91c630
DiegoPonte <diego.ponte@xpand-it.com>
2024-01-05 11:41:59 +0000

    Retarting flow to begin from first page

--- src/app/cwp-flow/services/cwp-flow-control.service.ts
+++ src/app/cwp-flow/services/cwp-flow-control.service.ts
@@ -16,7 +16,7 @@ export class CwpFlowControlService {
   readonly CWPFlowNavigationButtons = CWPFlowNavigationButtons;
   readonly GroupOfProfessions = GroupOfProfessions; // Reference to the enum in the component
   CWPFlowStepActive = new BehaviorSubject<CWPFlowStepsSequence>(
-    CWPFlowStepsSequence.contactDetails_phoneNumberPage
+    CWPFlowStepsSequence.personalData_salutationAndNamePage
   );
   residentLessThenTwoYears = new BehaviorSubject<boolean>(false);
   ageLessThen18Years = new BehaviorSubject<boolean>(false);
